acf8f685197b1ab6e1882804f774183940181d38
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-06 12:39:08 +0200

    Safe calculator decorator

---
+++ Logger/SafeCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class SafeCalculator:ICalculator
+    {
+        private Calculator _calculator;
+
+        public SafeCalculator(Calculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public int Add(int first, int second)
+        {
+            if (second == null) return 0;
+
+            return _calculator.Add(first, second);
+        }
+
+        public int Sub(int first, int second)
+        {
+            var res = _calculator.Sub(first, second);
+            Console.WriteLine($"{first} - {second} = {res}");
+            return res;
+        }
+    }
+}
